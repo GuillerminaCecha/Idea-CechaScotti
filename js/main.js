d7463c2126c37f16f40373ad4985e2b8db231671
Guillermina.Cecha <guillermina.cecha@gmail.com>
2024-02-11 09:59:20 -0300

    Fin del Proyecto Final Curso JavaScript

--- js/main.js
+++ js/main.js
@@ -7,18 +7,11 @@ fetch("./js/productos.json")
         cargarProductos(productos);
     })
 
-
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
 const numerito = document.querySelector("#numerito");
-
-
-botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
-    aside.classList.remove("aside-visible");
-}))
-
 
 function cargarProductos(productosElegidos) {
 
